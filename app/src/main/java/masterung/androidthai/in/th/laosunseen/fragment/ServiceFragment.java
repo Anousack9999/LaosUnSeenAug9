480993ee229d78236f4d2d760545178ec6f87683
Anousack <aousak.lass@gmail.com>
2018-08-09 17:30:33 +0700

    Initial commit

---
+++ app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/ServiceFragment.java
@@ -0,0 +1,36 @@
+package masterung.androidthai.in.th.laosunseen.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import masterung.androidthai.in.th.laosunseen.MainActivity;
+import masterung.androidthai.in.th.laosunseen.R;
+
+public class ServiceFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+    }   // Main Method
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_service, container, false);
+        return view;
+    }
+}
